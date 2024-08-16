1b034e4a3daf859d3b0856c032b76943f734be23
Fatimajabeenbangi1 <167059243+Fatimajabeenbangi1@users.noreply.github.com>
2024-08-16 13:02:45 +0530

    Add files via upload

---
+++ ArmstrongNum.java
@@ -0,0 +1,43 @@
+package java_pgm;
+
+import java.util.Scanner;
+public class ArmstrongNum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+     int no=sc.nextInt();
+     int t1=no;
+     int length=0;
+     while(t1 != 0)
+     {
+    	 length=length+1;
+    	 t1=t1/10;
+     }
+     int t2=no;
+     int arm=0;
+     while(t2 !=0)
+     {
+    	 int mul=1;
+    	 int rem = t2%10;
+    	 for(int i=1; i<=length;i++)
+    	 {
+    		 mul=mul*rem;
+    	 }
+    	 arm=arm+mul;
+    	 t2=t2/10;
+    	 
+     }
+     if(arm==no)
+     {
+    	 System.out.println(no+" is armstrong number");
+     }
+     else
+     {
+    	 System.out.println(no+"is not armstrong number");
+     }
+    	   	 
+    	
+	}
+
+}
